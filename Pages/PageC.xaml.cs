1ba60213dea0b0bb197e37418437d306b66f4bcf
aaronisberg <148402845+aaronisberg@users.noreply.github.com>
2023-12-20 18:37:40 -0800

    Navigation framework added

---
+++ Pages/PageC.xaml.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace NavigationBar.Pages
+{
+    /// <summary>
+    /// Interaction logic for Page3.xaml
+    /// </summary>
+    public partial class PageC : UserControl
+    {
+        public PageC()
+        {
+            InitializeComponent();
+            //This method won't work
+            //this.DataContext = new MainVM();
+        }
+    }
+}
